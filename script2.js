771957cca535cba297309761a6d543b22f4eb748
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-11-28 21:10:08 -0300

    Animais-Fantasticos-Origamid

---
+++ script2.js
@@ -0,0 +1,42 @@
+/* const animais = document.getElementById('animais')
+console.log(animais);
+
+const gridSection = document.getElementsByTagName('grid-section')
+console.log(gridSection[1]);
+
+const primeiraLi = document.querySelector('li')
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul')
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^=]"#"]');
+console.log(linkInterno.href);
+
+const animaisImg = document.querySelectorAll('.animais img');
+console.log(animaisImg[0]);
+
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
+
+primeiraUl.classList.add('grid-section');
+
+console.log(gridSectionHTML[0]);
+console.log(gridSectionNode[0]);
+
+gridSectionNode.forEach()
+ */
+
+// Retorne no console todas as imagens do site
+const img = document.querySelectorAll('img');
+console.log(img);
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+const imagensAnimais = document.querySelectorAll('img[src^="img/imagem"]');
+console.log(imagensAnimais);
+// Selecione todos os links internos (onde o href começa com #)
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+// Selecione o último p do site
+
